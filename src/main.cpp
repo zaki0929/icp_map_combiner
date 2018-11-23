46a4ff5f8b0862bf4331661980979c688733eb92
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2018-11-24 07:09:18 +0900

    Make the specification to draw three pointclouds

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 #include <pcl/point_cloud.h>
 #include <pcl/registration/icp.h>
 #include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/common/transforms.h>
 #include <Eigen/Dense>
 
-inline void print4x4Matrix (const Eigen::Matrix4d & matrix) {
+inline void print4x4Matrix (const Eigen::Matrix4d & matrix)
+{
   printf ("Rotation matrix :\n");
   printf ("    | %6.3f %6.3f %6.3f | \n", matrix (0, 0), matrix (0, 1), matrix (0, 2));
   printf ("R = | %6.3f %6.3f %6.3f | \n", matrix (1, 0), matrix (1, 1), matrix (1, 2));
@@ -14,42 +16,33 @@ inline void print4x4Matrix (const Eigen::Matrix4d & matrix) {
   printf ("t = < %6.3f, %6.3f, %6.3f >\n\n", matrix (0, 3), matrix (1, 3), matrix (2, 3));
 }
 
-boost::shared_ptr<pcl::visualization::PCLVisualizer> simpleVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud, pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud2)
+boost::shared_ptr<pcl::visualization::PCLVisualizer> threeCloudsVis (
+  pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud,
+  pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud2,
+  pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud3)
 {
-  // --------------------------------------------
-  // -----Open 3D viewer and add point cloud-----
-  // --------------------------------------------
   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
   viewer->setBackgroundColor (0, 0, 0);
-  viewer->addPointCloud<pcl::PointXYZ> (cloud, "cloud");
-  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud");
-  viewer->addPointCloud<pcl::PointXYZ> (cloud2, "cloud2");
-  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud2");
-  viewer->addCoordinateSystem (1.0);
-  viewer->initCameraParameters ();
-  return viewer;
-}
 
-boost::shared_ptr<pcl::visualization::PCLVisualizer> customColourVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud, pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud2)
-{
-  // --------------------------------------------
-  // -----Open 3D viewer and add point cloud-----
-  // --------------------------------------------
-  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-  viewer->setBackgroundColor (0, 0, 0);
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color(cloud, 0, 255, 0);
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color(cloud, 255, 0, 0);
   viewer->addPointCloud<pcl::PointXYZ> (cloud, single_color, "cloud");
   viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "cloud");
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color2(cloud2, 255, 0, 0);
+
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color2(cloud2, 0, 255, 0);
   viewer->addPointCloud<pcl::PointXYZ> (cloud2, single_color2, "cloud2");
   viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "cloud2");
+
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color3(cloud3, 0, 0, 255);
+  viewer->addPointCloud<pcl::PointXYZ> (cloud3, single_color3, "cloud3");
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "cloud3");
+
   viewer->addCoordinateSystem (1.0);
   viewer->initCameraParameters ();
   return (viewer);
 }
 
-int main (int argc, char** argv){
-
+int main (int argc, char** argv)
+{
   ros::init(argc, argv, "icp_map_combiner");
 
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
@@ -64,7 +57,6 @@ int main (int argc, char** argv){
   cloud_in->height   = 1;
   cloud_in->is_dense = false;
   cloud_in->points.resize (cloud_in->width * cloud_in->height);
-
   for (size_t i = 0; i < cloud_in->points.size(); ++i) {
     cloud_in->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
     cloud_in->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
@@ -82,20 +74,21 @@ int main (int argc, char** argv){
   pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
   icp.setInputCloud(cloud_in);
   icp.setInputTarget(cloud_out);
-
-  pcl::PointCloud<pcl::PointXYZ> final_cloud;
-  icp.align(final_cloud);
+  pcl::PointCloud<pcl::PointXYZ> cloud_source_registered;
+  icp.align(cloud_source_registered);
 
   // 変換行列を表示
   Eigen::Matrix4d transformation_matrix = Eigen::Matrix4d::Identity ();
   transformation_matrix = icp.getFinalTransformation ().cast<double>();
   print4x4Matrix (transformation_matrix);
 
-  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
-  //viewer = simpleVis(cloud_in, cloud_out);
-  viewer = customColourVis(cloud_in, cloud_out);
+  // 変換行列から ICP で推定された点群 cloud_final を取得
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::transformPointCloud(*cloud_in, *cloud_final, transformation_matrix); 
 
-  // 待機
+  // 点群の可視化
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+  viewer = threeCloudsVis(cloud_in, cloud_out, cloud_final);
   while (!viewer->wasStopped()) {
     viewer->spinOnce (100);
     boost::this_thread::sleep (boost::posix_time::microseconds (100000));
